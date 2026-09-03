d3b257d8688fec41f056a6f13a9ba36adb8a6bf8
Starlight Unit Studios <pschildgen87@gmail.com>
2026-09-04 00:06:27 +0200

    Set security contract alpha version

--- internal/buildinfo/buildinfo.go
+++ internal/buildinfo/buildinfo.go
@@ -2,7 +2,7 @@ package buildinfo
 
 // These values may be replaced with -ldflags during release builds.
 var (
-	Version   = "0.2.0-alpha.1"
+	Version   = "0.2.0-alpha.2"
 	Commit    = "unknown"
 	BuildTime = "unknown"
 )
